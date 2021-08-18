3ef5e74d5c4ed7e71920f675f701b90c9a0cf2ea
JonnHenry <jonnathanca9601@gmail.com>
2021-08-17 23:42:59 -0500

    Changues in the files

--- src/main/java/com/cursojava/curso/models/User.java
+++ src/main/java/com/cursojava/curso/models/User.java
@@ -1,26 +1,29 @@
 package com.cursojava.curso.models;
 import lombok.Data;
-import org.springframework.data.annotation.Id;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Entity //The objective of this notation is access to DB
 @Table(name = "Users") //Reference to the table Users in the DB
 @Data //Implicits @Getter and @Setter @HashCode and @ToString
-public class User {
+public class User{
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
     @Column(name = "id") //To specify the column in the table
-    @Id //To specify that is a primary key
-    private Long id;
+    private Long id;//To specify that is a primary key
+
     @Column(name = "name")
     private String name;
+
     @Column(name = "lastName")
     private String lastName;
+
     @Column(name = "email")
     private String email;
+
     @Column(name = "phone")
     private String phone;
+
     @Column(name = "password")
     private String password;
 }
